6d3dd52e7d925d8dc29ed61f3b3b1e6eca7bc11c
Fábio Santos <fabiosantosart@gmail.com>
2014-06-23 12:18:41 +0100

    start building a JS file instead of an HTML file

--- half-build.sh
+++ half-build.sh
@@ -4,6 +4,9 @@ set +e
 
 cd js_build
 
-cp bin/cycles_test cycles_test.bc && emcc --embed-file elephant.xml --embed-file gumbo.xml lib/libcycles_kernel.so lib/libcycles_util.so lib/libcycles_device.so lib/libcycles_bvh.so lib/libcycles_subd.so  lib/libcycles_render.so cycles_test.bc -O2 -o cycles_test.html
-# cp bin/cycles_test cycles_test.bc && emcc --embed-file elephant.xml --embed-file gumbo.xml lib/libcycles_kernel.so lib/libcycles_util.so lib/libcycles_device.so lib/libcycles_bvh.so lib/libcycles_subd.so  lib/libcycles_render.so cycles_test.bc --llvm-lto 1 -o cycles_test.html
-# cp bin/cycles_test cycles_test.bc && emcc --embed-file elephant.xml --embed-file gumbo.xml cycles_test.bc -o cycles_test.html
+# HTML build
+# cp bin/cycles_test cycles_test.bc && emcc --embed-file elephant.xml --embed-file gumbo.xml lib/libcycles_kernel.so lib/libcycles_util.so lib/libcycles_device.so lib/libcycles_bvh.so lib/libcycles_subd.so  lib/libcycles_render.so cycles_test.bc -O2 -o cycles_test.html
+
+# JavaScript build
+cp bin/cycles_test cycles_test.bc && emcc --embed-file elephant.xml --embed-file gumbo.xml lib/libcycles_kernel.so lib/libcycles_util.so lib/libcycles_device.so lib/libcycles_bvh.so lib/libcycles_subd.so  lib/libcycles_render.so cycles_test.bc -O2 -o cycles_test.js
+
