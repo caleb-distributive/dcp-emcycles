36e2e7cfadac669bd7312a840d0fb6788d4ffeaf
Fábio Santos <fabiosantosart@gmail.com>
2014-07-19 11:37:28 +0100

    move the _main() call to the bottom of the program instead of relying on callbacks and events just use sync xhr because we are in a web worker

--- cloudrender_init.js
+++ cloudrender_init.js
@@ -6,30 +6,6 @@ if (typeof Module !== 'undefined') var outerModule = Module
 Module = outerModule || {}  // It's defined again, by emscripten code
 
 Module.noInitialRun = true
-Module.preInit = function () {
-    var toDo = INCLUDES.length
-    function tryDone() {
-        toDo--;
-        if (!toDo) Module._main();
-    }
-
-    if (/\.xml$/.test(SCENE)) {
-        toDo++
-        load(SCENE, function () {
-            FS.writeFile('scene.xml', this.responseText)
-            tryDone()
-        })
-    } else {
-        FS.writeFile('scene.xml', SCENE)
-    }
-
-    INCLUDES.forEach(function (include) {
-        load(include, function () {
-            FS.writeFile(include, this.responseText)
-            tryDone()
-        })
-    })
-}
 
 Module.draw_out = Module.draw_out || function (mem, w, h, pix) {
     console.log('draw_out!')
@@ -43,11 +19,4 @@ Module.draw_out = Module.draw_out || function (mem, w, h, pix) {
 }
 
 Module.TOTAL_MEMORY = 256 * 1024 * 1024 // 64mb
-
-function load(url, cb) {
-    var xhr = new XMLHttpRequest()
-    xhr.onload = cb
-    xhr.open('get', url, true)
-    xhr.send()
-}
 
