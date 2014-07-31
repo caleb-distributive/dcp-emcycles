4e8cee3f8370a0f12ef0e833643d605c556bd353
Fábio Santos <fabiosantosart@gmail.com>
2014-08-01 00:13:23 +0100

    add third-party png.js and zlib.js to the built thing

--- cat-build.sh
+++ cat-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat cloudrender_init.js js_build/cycles_test.js cloudrender_close.js > ./cloudrender_core.js
+cat cloudrender_init.js zlib.js png.js js_build/cycles_test.js cloudrender_close.js > ./cloudrender_core.js
 
 touch index.html
